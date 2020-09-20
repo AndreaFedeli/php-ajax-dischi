28a5fe81d78d26dda4a54f3f2157384ba380b0e5
AndreaFedeli <andrea.fedeli159@gmail.com>
2020-09-20 15:27:02 +0200

    php def

--- index.php
+++ index.php
@@ -5,16 +5,30 @@ include 'db.php';
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.2/handlebars.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="dist/app.css">
         <title></title>
     </head>
     <body>
-        <?php foreach ($database as $key) { ?>
-        <ul>
-            <li><?php echo $key['title']?></li>
-            <li><?php echo  $key['author']?></li>
-            <li><?php echo $key['year']?></li>
-            <li><img src="<?php echo $key['poster']?>" alt=""></li>
-        </ul>
+
+      <header>
+        <div class="container">
+        <img src="img/logo.png" alt="logo">
+        </div>
+
+        <?php foreach ($database as $value) { ?>
+        <div class="cds-container container">
+          <div class="cd">
+            <img src="<?php echo $value['poster']?>" alt="">
+            <h3><?php echo $value['title']?></h3>
+            <span class="author"><?php echo  $value['author']?></span>
+            <span class="year"><?php echo $value['year']?></span>
+
+        </div>
         <?php } ?>
+      </div>
+
     </body>
 </html>
