badcffd763282f1b6a4f14d99716e61fcb8671dc
AndreaFedeli <andrea.fedeli159@gmail.com>
2020-09-21 12:13:02 +0200

    update

---
+++ src/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+$.ajax(
+  {
+    'url': 'http://localhost/repo-php-ajax-dischi/db.php',
+    'method': "GET",
+    'success': function (database) {
+      var source = $('#entry-template').html();
+      var template = Handlebars.compile(source);
+      for (var i = 0; i < database.length; i++) {
+        var context= {
+          "title" : database[i].title,
+          "author" : database[i].author,
+          "year" : database[i].year,
+          "poster" : database[i].poster,
+        };
+        var html = template(context);
+        $(".box").append(html);
+      }
+    },
+    'error': function () {
+      alert("errore");
+    }
+  }
+);
+})
